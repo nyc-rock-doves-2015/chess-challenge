76b5f6830616709e496ef516b9697d6797310aee
Harry Park <imharrypark@gmail.com>
2015-03-26 18:09:22 -0400

    to_s method displays chess board. Still need to differentiate black and white

--- chess-control.rb
+++ chess-control.rb
@@ -2,7 +2,8 @@ require_relative 'chess-model'
 require_relative 'chess-view'
 
 class Control
-  attr_reader :model, :view
+  attr_reader :view, :model
+
   def initialize
     @view = View.new
     @model = Board.new
@@ -19,10 +20,23 @@ class Control
   def runner
     @view.prompt_move
     place
+    # @view.prompt_move
+    # place
+    # @view.prompt_move
+    # place
+    # @view.prompt_move
+    # place
+    # @view.prompt_move
+    # place
+    # @view.prompt_move
+    # place
+    # @view.prompt_move
+    # place
+    @view.to_s(@model.board)
   end
 
 end
 
-# control = Control.new
-# control.runner
+control = Control.new
+control.runner
 # p control.model.board
--- chess-view.rb
+++ chess-view.rb
@@ -3,6 +3,7 @@ class View
 
   def initialize
   end
+
   def prompt_move
     puts "Which piece would you like to place? (King, Queen, Bishop, Knight, Rook, Pawn)"
     @piece = gets.chomp
@@ -11,4 +12,45 @@ class View
     puts "Where would you like to place the piece? Use chess notation (i.e. a2, e7..)"
     @location = gets.chomp
   end
+
+  def to_s(hash)
+    board_array = []
+    hash.values.each_slice(8) do |pieces|
+      board_array << pieces
+    end
+    board_array << ['a','b','c','d','e','f','g','h']
+    row_num = 8
+    board_array.each do |row|
+      if row_num > 0
+        print row_num.to_s + "  "
+      else
+        print "     "
+      end
+      row.each do |piece|
+        if piece.is_a? Rook
+          print "♜"
+        elsif piece.is_a? Knight
+          print "♞"
+        elsif piece.is_a? Bishop
+          print "♝"
+        elsif piece.is_a? Queen
+          print "♛"
+        elsif piece.is_a? King
+          print "♚"
+        elsif piece.is_a? Pawn
+          print "♟"
+        elsif piece == nil
+          print "  "
+        else
+          print piece
+        end
+        print "  "
+      end
+      row_num -= 1
+      puts
+    end
+  end
 end
+
+
+# view = View.new
