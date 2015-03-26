3fb23504f7aebfade8112f54f52c5b8300754494
Harry Park <imharrypark@gmail.com>
2015-03-26 17:09:36 -0400

    added a runner method for Control, prompt_move method for View, successfully placing a piece on a test board.

--- chess-view.rb
+++ chess-view.rb
@@ -1,2 +1,14 @@
 class View
+  attr_accessor :piece, :location, :color
+
+  def initialize
+  end
+  def prompt_move
+    puts "Which piece would you like to place? (King, Queen, Bishop, Knight, Rook, Pawn)"
+    @piece = gets.chomp
+    puts "Which color? (Black or White)"
+    @color = gets.chomp
+    puts "Where would you like to place the piece? Use chess notation (i.e. a2, e7..)"
+    @location = gets.chomp
+  end
 end
